d8fca978aa3987a1d9eb471cc6bd83889aa491d1
Muhammed Chreiki <mahmibey@gmail.com>
2025-12-21 13:34:11 +0300

    Last files

---
+++ Meta/Fullstack/React Basics/React/src/App.tsx
@@ -0,0 +1,53 @@
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
+
+/* React Router (react-router-dom) */
+
+// const Homepage = () => {
+//   return (
+//     <div>
+//       <h1 className="m-5 font-bold">You Are In The Homepage</h1>
+//     </div>
+//   );
+// };
+
+// const Javascript = () => {
+//   return (
+//     <div>
+//       <h1 className="m-5 font-bold">You Are In Javascript Page</h1>
+//     </div>
+//   );
+// };
+
+// const Html = () => {
+//   return (
+//     <div>
+//       <h1 className="m-5 font-bold">You Are In Html Page</h1>
+//     </div>
+//   );
+// };
+
+// import { Routes, Route, Link } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <nav className="flex gap-10 p-4 bg-amber-400 w-full">
+//         <Link to="/">Homepage</Link>
+//         <Link to="/javascript">Javascript</Link>
+//         <Link to="/html">Html</Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<Homepage />}></Route>
+//         <Route path="/javascript" element={<Javascript />}></Route>
+//         <Route path="/html" element={<Html />}></Route>
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
