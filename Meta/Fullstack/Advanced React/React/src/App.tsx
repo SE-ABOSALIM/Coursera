4240fc297a6bbe9cbf7336e5b6216a0e3f5a298c
Muhammed Chreiki <mahmibey@gmail.com>
2025-12-28 00:47:49 +0300

    Custom Hook

--- Meta/Fullstack/Advanced React/React/src/App.tsx
+++ Meta/Fullstack/Advanced React/React/src/App.tsx
@@ -4,6 +4,48 @@ const App = () => {
 
 export default App;
 
+/* Custom Hook */
+// import { useState, useEffect, useRef } from "react";
+
+// const App = () => {
+//   const [day, setDay] = useState("Monday");
+//   const prevDay = usePrevious(day);
+//   const getNextDay = () => {
+//     if (day === "Monday") {
+//       setDay("Tuesday");
+//     } else if (day === "Tuesday") {
+//       setDay("Wednesday");
+//     } else if (day === "Wednesday") {
+//       setDay("Thursday");
+//     } else if (day === "Thursday") {
+//       setDay("Friday");
+//     } else if (day === "Friday") {
+//       setDay("Monday");
+//     }
+//   };
+
+//   return (
+//     <div style={{ padding: "40px" }}>
+//       <h1>
+//         Today is: {day}
+//         <br />
+//         {prevDay && <span>Previous work day was: {prevDay}</span>}
+//       </h1>
+//       <button onClick={getNextDay}>Get next day</button>
+//     </div>
+//   );
+// };
+
+// const usePrevious = (val: string) => {
+//   const ref = useRef("");
+//   useEffect(() => {
+//     ref.current = val;
+//   }, [val]);
+//   return ref.current;
+// };
+
+// export default App;
+
 /* useRef example */
 // import { useRef } from "react";
 
