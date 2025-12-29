7f2e1d7c1cdb7a30ef043e0d206fb5e5490605fd
Muhammed Chreiki <mahmibey@gmail.com>
2025-12-29 16:18:05 +0300

    Complete Later

--- Meta/Fullstack/Advanced React/React/src/App.tsx
+++ Meta/Fullstack/Advanced React/React/src/App.tsx
@@ -1,5 +1,21 @@
+import "./App.css";
+import { RadioGroup, RadioOption } from "./components";
+import { useState } from "react";
+
 const App = () => {
-  return <div></div>;
+  const [selected, setSelected] = useState("");
+  return (
+    <div className="App">
+      <h2>How did you hear about Little Lemon?</h2>
+      <RadioGroup onChange={setSelected} selected={selected}>
+        <RadioOption value="social_media">Social Media</RadioOption>
+        <RadioOption value="friends">Friends</RadioOption>
+        <RadioOption value="advertising">Advertising</RadioOption>
+        <RadioOption value="other">Other</RadioOption>
+      </RadioGroup>
+      <button disabled={!selected}>Submit</button>
+    </div>
+  );
 };
 
 export default App;
