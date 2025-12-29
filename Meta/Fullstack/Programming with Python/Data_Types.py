0ead84de5215af07329fe214fb583c419c71c5b0
Muhammed Chreiki <mahmibey@gmail.com>
2025-12-29 17:35:39 +0300

    New Directory for Python, Data_Types has been discovered

---
+++ Meta/Fullstack/Programming with Python/Data_Types.py
@@ -0,0 +1,33 @@
+""" Numeric Data Types """
+a = 12
+b = 2.2
+c = 2 + 3j
+
+print(a)
+print(b)
+print(c)
+print()
+print(type(a))
+print(type(b))
+print(type(c))
+print()
+print()
+
+""" Sequence Data Types """
+String = "Hello, World!" # String
+List = [1,2,3,4] # Simple Array, Arrays are mutable
+Tuple = (5,6,7,8) # Tuples are immutable
+Set = {10, 12, 'hello', 'hello', 'Hello', True, True, False, 2.2, 3.4, 2.2} # Set returns only unique values
+dictionary = {'key1':1,'key2':2,'key3':3} # For Key-Value Pairs
+
+print(String)
+print(List)
+print(Tuple)
+print(Set)
+print(dictionary['key3'])
+print()
+print(type(String))
+print(type(List))
+print(type(Tuple))
+print(type(Set))
+print(type(dictionary))
