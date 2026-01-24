7a8ce7653cf92c9398875ef44950a1e4130fb969
Muhammed Chreiki <mahmibey@gmail.com>
2026-01-24 19:12:18 +0300

    Upper case comprehension

--- Meta/Fullstack/Programming with Python/Scripts/Comprehension.py
+++ Meta/Fullstack/Programming with Python/Scripts/Comprehension.py
@@ -22,7 +22,9 @@ print(f'New list (Divisible by four minus one): {mod_four}')
 nines = [x for x in range(100) if x%7 == 0]
 print(f'From 1-100 but the divisible by 9 ones {nines}')
 
-
+lst2 = ['hello', 'i', 'love', 'rocket', 'league']
+upper_case = [word.upper() for word in lst2]
+print(f'Rocket League: {upper_case}')
 
 # Dictionary comprehension
 print(
