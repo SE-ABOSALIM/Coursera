4bb1b06ed8701abe177b4677c1a64840f602b904
Muhammed Chreiki <mahmibey@gmail.com>
2026-02-23 15:30:57 +0300

    filtering, ordering, searching and pagination practices

--- Meta/Fullstack/APIs/DRF Practices/project/Serialization/serializers.py
+++ Meta/Fullstack/APIs/DRF Practices/project/Serialization/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 from .models import Food, MenuItem, Category
 from decimal import Decimal
+import bleach
 
 class FoodsSerializer(serializers.ModelSerializer):
     country = serializers.CharField(source='origin')
@@ -26,19 +28,70 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'slug', 'title']
 
 class MenuItemSerializer(serializers.ModelSerializer): # PASS HERE
-    stock = serializers.IntegerField(source='inventory')
-    calculated_tax = serializers.SerializerMethodField()
-    # This part creates a hyperlink refers to name='category-detail' named url. Instead of that, it could be done by passing (serializers.HyperlinkedModelSerializer) to "PASS HERE"
-    category = serializers.HyperlinkedRelatedField (
-        queryset = Category.objects.all(),
-        view_name='category-detail',
+    title = serializers.CharField( # METHOD 1 (Unique Validator): pass to field itself
+        max_length=255,
+        validators=[UniqueValidator(queryset=MenuItem.objects.all())]
     )
-    # category = CategorySerializer(read_only=True) # uncomment to perform post request in api/menu-items and cpmment the above one
+    stock = serializers.IntegerField(source='inventory', min_value=1)
+    calculated_tax = serializers.SerializerMethodField()
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=10) # METHOD 1 (Conditions): Adding condition to price field (cannot be less than 10.0)
+    category = CategorySerializer(read_only=True) # to be able to perform post request in api/menu-items uncomment this line and comment below part
+    # category = serializers.HyperlinkedRelatedField ( # This part creates a hyperlink refers to name='category-detail' named url. Instead of that, it could be done by passing (serializers.HyperlinkedModelSerializer) to "PASS HERE"
+    #     queryset = Category.objects.all(),
+    #     view_name='category-detail',
+    # )
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'stock',  'calculated_tax', 'category']
         # depth=1 # same as (category = CategorySerializer()). This way you don't need the CategorySerializer
+        # extra_kwargs = { # METHOD 2 (Conditions): Rather than defining and adding conditions to the field we can add conditions using (extra_kwargs)
+        #     'price': {'min_value': 2}
+        # }
+
+        # METHOD 2 (Unique Validator): Define it in extra_kwargs
+        # extra_kwargs = {
+        #     'title': {
+        #         'validators': [
+        #             UniqueValidator(
+        #                 queryset=MenuItem.objects.all()
+        #             )
+        #         ]
+        #     }
+        # }
+
+        # Unique Validator Togather: You can define more than one field to validate unique values
+        # validators = [
+        #     UniqueTogetherValidator(
+        #         queryset=MenuItem.objects.all(),
+        #         fields=['title', 'price'],
+        #     )
+        # ]
 
     def get_calculated_tax(self, product:MenuItem):
         taxed_price = product.price * Decimal('1.10')
         return taxed_price.quantize(Decimal('0.00'))
+    
+    #METHOD 3 (Conditions): Defining conditions with function
+    # def validate_price(self, value):
+    #     if value < 10:
+    #         return serializers.ValidationError('Price should not be less than 10.0')
+    #     return value
+
+    #METHOD 4 (Conditions): Defining conditions with function and attrs parameter | Sanitize title field
+    # def validate(self, attrs):
+    #     attrs['title'] = bleach.clean(attrs['title'])
+    #     if(attrs['price']<2):
+    #         raise serializers.ValidationError('Price should not be less than 2.0')
+    #     if(attrs['inventory']<0):
+    #         raise serializers.ValidationError('Stock cannot be negative')
+    #     return super().validate(attrs)
+
+    # Sanitizes user input by cleaning HTML tags and attributes to mitigate XSS (Cross-Site Scripting) attacks.
+    """
+    If we try to inject HTML here is the result:
+    injected field    ->     title = Baklava <script>alert('Hello')</script> Dessert
+    Output            ->    "title": "Baklava &lt;script&gt;alert('Hello')&lt;/script&gt; Dessert",
+
+    """
+    def validate_title(self, value):
+        return bleach.clean(value)
--- Meta/Fullstack/APIs/DRF Practices/project/Serialization/views.py
+++ Meta/Fullstack/APIs/DRF Practices/project/Serialization/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework import status
+from django.core.paginator import Paginator, EmptyPage
 from .models import Food, MenuItem, Category
 from .serializers import ( 
     FoodsSerializer, 
@@ -49,6 +50,29 @@ def foods_menu_item_filtered(request, pk):
 def menu_items(request):
     if request.method == 'GET':
         items = MenuItem.objects.select_related('category').all()
+        # Searching | Filtering | Ordering | Pagination splits large datasets into smaller pages to improve performance and make API responses easier to consume.
+        category_name = request.query_params.get('category') # filter parameter
+        price = request.query_params.get('price') # filter parameter
+        search = request.query_params.get('search') # search parameter
+        order = request.query_params.get('order') # Order parameter
+        perpage = request.query_params.get('perpage', default=2)
+        page = request.query_params.get('page', default=1)
+        if category_name: # title(model-field)__lookup(rule-to-apply)
+            items = items.filter(category__title=category_name) # filtering based on category name
+        if price: 
+            items = items.filter(price=price) # filtering based on price
+        if search: 
+            items = items.filter(title__icontains=search) # searching based on url search parameter's value
+        if order:
+            # items = items.order_by(order) # ordering based on a model field
+            order_fields = order.split(',')
+            items = items.order_by(*order_fields) # ordering based on more than one model field togather
+        # ----------------------------------------------------------------------------------------------------
+        paginator = Paginator(items, per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except:
+            items = []
         serialized_items = MenuItemSerializer(items, many=True, context={'request': request}) # we need to provide the context so DRF could extract url field like (port, host, scheme, etc.) from the request object
         return Response(serialized_items.data)
     # To make that work go to serializers.py in the MenuItemSerializer class comment the first category variable and uncomment the second
