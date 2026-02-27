0bfc40e79b3ac4b9d233ded677a9c347f69d3554
Muhammed Chreiki <mahmibey@gmail.com>
2026-02-27 22:18:27 +0300

    mapping the order endpoint urls

--- Meta/Fullstack/APIs/DRF Practices/project/FinalProject/urls.py
+++ Meta/Fullstack/APIs/DRF Practices/project/FinalProject/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('menu-items', views.menu_items, name='menu-items'),
     path('menu-items/<int:pk>', views.menu_item_details, name='menu-items-detail'),
 
-    # path('orders'),
-    # path('orders/<int:id>'),
+    path('orders', views.orders),
+    path('orders/<int:id>', views.order_details),
     path('cart/menu-items', views.user_cart),
 
     path('groups/<str:group_name>/users', views.group_users),
